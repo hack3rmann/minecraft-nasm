35b83c0cfdfb6681262ec3855ca0661d9731b7c7
Svyatoslav <slava-varov3@mail.ru>
2025-11-17 14:53:57 +0300

    add print_cstr function

--- src/main.asm
+++ src/main.asm
@@ -11,14 +11,57 @@ section .rodata
 
     hello_world     db "Hello, World!", LF
     hello_world.len equ $-hello_world
+    cstring         db "This is a C-string!!!", LF, 0
+
+section .bss
+    argc resq 1
+    argc_string resb 16
 
 section .text
+
+; #[fastcall(al, rdx)]
+; fn cstr_len((ptr := rsi): *const u8) -> usize := rdx
+cstr_len:
+    ; let (result := rdx) = 0
+    xor rdx, rdx
+
+    ; while *(ptr + result) != null {
+    .while:
+    mov al, byte [rsi+rdx]
+    test al, al
+    jz .end_while
+
+        ; result += 1
+        inc rdx
+
+    ; }
+    jmp .while
+    .end_while:
+
+    ret
+
+; #[fastcall]
+; fn print_cstr((ptr := rsi): *const u8)
+print_cstr:
+    ; let (len := rdx) = cstr_len(ptr)
+    call cstr_len
+
+    ; write(STDOUT, ptr, len)
+    mov rax, SYSCALL_WRITE
+    mov rdi, STDOUT
+    syscall
+
+    ret
 
 ; #[fastcall]
 ; #[noreturn]
 ; fn start() -> !
 global start
 start:
+    ; argc = get_argc_from_stack()
+    mov rax, qword [rsp]
+    mov qword [argc], rax
+
     ; let (exit_code := rax) = main()
     call main
 
@@ -31,12 +74,9 @@ start:
 ; fn main() -> i32
 global main
 main:
-    ; write(STDOUT, hello_world, hello_world_len)
-    mov rax, SYSCALL_WRITE
-    mov rdi, STDOUT
-    mov rsi, hello_world
-    mov rdx, hello_world.len
-    syscall
+    ; print_cstr(cstring)
+    mov rsi, cstring
+    call print_cstr
 
     ; return EXIT_SUCCESS
     xor rax, rax
