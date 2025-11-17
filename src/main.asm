898680f4da8d872def8281b689c6f75c3e52568c
Svyatoslav <slava-varov3@mail.ru>
2025-11-17 15:23:12 +0300

    add environment pointers

--- src/main.asm
+++ src/main.asm
@@ -14,10 +14,73 @@ section .rodata
     cstring         db "This is a C-string!!!", LF, 0
 
 section .bss
+    ; static argc: usize
     argc resq 1
-    argc_string resb 16
+    ; static argv: *const *const u8
+    argv resq 1
+    ; static envp: *const *const u8
+    envp resq 1
 
 section .text
+
+; #[nocall]
+; #[noreturn]
+; fn start() -> !
+global start
+start:
+    ; Higher addresses
+    ; ┌───────────────────┐
+    ; │ ...               │
+    ; ├───────────────────┤
+    ; │ NULL              │ ← End of environment pointers
+    ; ├───────────────────┤
+    ; │ ...               │
+    ; │ envp[2]           │ ← Environment variable strings  
+    ; │ envp[1]           │   (each is "NAME=VALUE")
+    ; │ envp[0]           │
+    ; ├───────────────────┤
+    ; │ argv[argc] = NULL │ ← End of argument pointers  
+    ; │ ...               │
+    ; │ argv[1]           │ ← Command-line arguments
+    ; │ argv[0]           │ ← Program name
+    ; ├───────────────────┤
+    ; │ argc              │ ← Argument count (integer)
+    ; └───────────────────┘
+    ; Lower addresses
+
+    ; argc = get_argc_from_stack()
+    mov rax, qword [rsp]
+    mov qword [argc], rax
+
+    ; argv = get_argv_from_stack()
+    lea rax, [rsp+8]
+    mov qword [argv], rax
+
+    ; envp = get_envp_from_stack()
+    mov rax, qword [argc]
+    lea rax, [8*rax+16+rsp]
+    mov qword [envp], rax
+
+    ; let (exit_code := rax) = main()
+    call main
+
+    ; exit(exit_code)
+    mov rdi, rax
+    mov rax, SYSCALL_EXIT
+    syscall
+
+; #[fastcall(all)]
+; fn main() -> i64
+global main
+main:
+    ; print_cstr(envp[0])
+    mov rsi, qword [envp]
+    mov rsi, qword [rsi+6*8]
+    call print_cstr
+
+    ; return EXIT_SUCCESS
+    xor rax, rax
+    ret
 
 ; #[fastcall(al, rdx)]
 ; fn cstr_len((ptr := rsi): *const u8) -> usize := rdx
@@ -40,7 +103,7 @@ cstr_len:
 
     ret
 
-; #[fastcall]
+; #[fastcall(rdx, rax, rdi)]
 ; fn print_cstr((ptr := rsi): *const u8)
 print_cstr:
     ; let (len := rdx) = cstr_len(ptr)
@@ -51,33 +114,4 @@ print_cstr:
     mov rdi, STDOUT
     syscall
 
-    ret
-
-; #[fastcall]
-; #[noreturn]
-; fn start() -> !
-global start
-start:
-    ; argc = get_argc_from_stack()
-    mov rax, qword [rsp]
-    mov qword [argc], rax
-
-    ; let (exit_code := rax) = main()
-    call main
-
-    ; exit(exit_code)
-    mov rdi, rax
-    mov rax, SYSCALL_EXIT
-    syscall
-
-; #[fastcall]
-; fn main() -> i32
-global main
-main:
-    ; print_cstr(cstring)
-    mov rsi, cstring
-    call print_cstr
-
-    ; return EXIT_SUCCESS
-    xor rax, rax
     ret
