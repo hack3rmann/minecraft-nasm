c70680193ac3cda6673d6f42434b1b59a0d652ff
Svyatoslav <slava-varov3@mail.ru>
2025-12-08 01:36:20 +0300

    add fn macro docs

--- src/function.s
+++ src/function.s
@@ -3,6 +3,7 @@
 
 %define N_PUSHAS 16
 
+; PUSH <REGISTERS>,*
 %macro PUSH 0-*
     %rep %0
         push %1
@@ -11,6 +12,7 @@
     %endrep
 %endmacro
 
+; POP <REGISTERS>,*
 %macro POP 0-*
     %rep %0
         POP %1
@@ -20,19 +22,25 @@
     %assign .push_size 0
 %endmacro
 
+; PUSHA
 %macro PUSHA 0
     pushf
     PUSH rax, rcx, rdx, rbx, rsp, rbp, rsi, rdi, \
          r8, r9, r10, r11, r12, r13, r14, r15
+
+    %assign .push_size .push_size+8
 %endmacro
 
+; POPA
 %macro POPA 0
     POP r15, r14, r13, r12, r11, r10, r9, r8, rdi, rsi, rbp, \
         rsp, rbx, rdx, rcx, rax
     popf
 %endmacro
 
-; FUNCTION <NAME>
+; FN <NAME>
+;     ...
+; END_FN
 %macro FN 1
 %1:
     push rbp
@@ -42,18 +50,23 @@
     %assign .local_size 0
 %endmacro
 
+; FN <NAME>
+;     ...
+; END_FN
 %macro END_FN 0
     mov rsp, rbp
     pop rbp
     ret
 %endmacro
 
+; LOCAL <NAME> <SIZE>
 %macro LOCAL 2
     %1 equ -%2 - .local_size - .push_size
 
     %assign .local_size .local_size+%2
 %endmacro
 
+; STACK <SIZE_NAME>
 %macro STACK 1
     %1 equ ALIGNED(.local_size)
 %endmacro
